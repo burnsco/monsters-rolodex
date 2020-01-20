4e8d81b91ebf4f636ad47c5af570dd7d6ba9498d
Corey Burns <coreymburns@gmail.com>
2020-01-20 14:21:02 -0500

    styled component card finish

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,3 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const Card = ({ user }) => <h1>{user.name}</h1>
+const Container = styled.h1`
+  display: flex;
+  flex-direction: column;
+  background-color: #95dada;
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 25px;
+  cursor: pointer;
+  -moz-osx-font-smoothing: grayscale;
+  backface-visibility: hidden;
+  transform: translateZ(0);
+  transition: transform 0.25s ease-out;
+  &:hover {
+    transform: scale(1.05);
+  }
+`
+
+export const Card = ({ user }) => <Container>{user.name}</Container>
