85a006858bb1f73ade724799a696ef0fc3e53a8e
Corey Burns <coreymburns@gmail.com>
2020-01-22 12:40:18 -0500

    Updates

---
+++ precache-manifest.db9986708825994a84b2b924e69d706e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85861e4e75173743432ec2d55ac60c1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a6328286ee1839fce2a2",
+    "url": "/monsters-rolodex/static/css/main.c7d73262.chunk.css"
+  },
+  {
+    "revision": "f1d639a43023a618d613",
+    "url": "/monsters-rolodex/static/js/2.63fe5844.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/monsters-rolodex/static/js/2.63fe5844.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6328286ee1839fce2a2",
+    "url": "/monsters-rolodex/static/js/main.615886c1.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
